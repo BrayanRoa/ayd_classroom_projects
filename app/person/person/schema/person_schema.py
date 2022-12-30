09cc4e044b654984b3fbc3d73b6813aab32b02e3
brayanroa <brayanandresrl@ufps.edu.co>
2022-12-29 20:16:06 -0500

    validations person added

--- app/person/person/schema/person_schema.py
+++ app/person/person/schema/person_schema.py
@@ -1,14 +1,22 @@
 from app.ext import ma
-from marshmallow import fields
+from marshmallow import fields, validate
+
 
 class PersonSchema(ma.Schema):
-    institutional_mail=fields.String()
-    password = fields.String()
-    names= fields.String()
-    lastnames= fields.String()
-    code =  unique=fields.String()
-    document_type_id = fields.Integer()
-    role_id=fields.Integer()
-   
-person_schema=PersonSchema()
-persons_schema=PersonSchema(many=True)
+    institutional_mail = fields.Email(
+        required=True,
+        error_messages={
+            "required": "Email is mandatory field.",
+            "invalid": "The email is not valid.",
+        },
+    )
+    password = fields.String(required=True)
+    names = fields.String(required=True)
+    lastnames = fields.String(required=True)
+    code = unique = fields.String(validate=validate.Length(min=7, max=8))
+    document_type_id = fields.Integer(required=True)
+    role_id = fields.Integer(required=True)
+
+
+person_schema = PersonSchema()
+persons_schema = PersonSchema(many=True, exclude=["password"])
